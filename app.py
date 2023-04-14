a4d23c6844ab5da6797293045492899a7c6e974c
Juan <juanmontalvobressi@hotmail.com>
2023-04-14 17:18:15 -0300

    auto pep8 test

--- app.py
+++ app.py
@@ -25,16 +25,26 @@ socketio = SocketIO(app)
 BUFFER_SIZE = 1024
 PORT_NUMBER = 12345
 
-# This function handles incoming netcat connections and displays the received data in real-time
+# This function handles incoming netcat connections and displays the
+# received data in real-time
+
+
 def handle_client(client_socket, client_address):
     # Receive data from the client
     data = client_socket.recv(BUFFER_SIZE)
     # Print the received data to the console
     print(f"Received data from {client_address}: {data.decode('utf-8')}")
-    # Send the received data to connected clients using SocketIO, along with IP address and timestamp
-    socketio.emit("message", {"ip": client_address[0], "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "content": data.decode('utf-8')})
+    # Send the received data to connected clients using SocketIO, along with
+    # IP address and timestamp
+    socketio.emit("message",
+                  {"ip": client_address[0],
+                   "timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                   "content": data.decode('utf-8')})
 
-# This function starts a listener for incoming netcat connections on the specified port
+# This function starts a listener for incoming netcat connections on the
+# specified port
+
+
 def start_netcat_listener(port):
     # Create a socket for the server
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -46,22 +56,30 @@ def start_netcat_listener(port):
     while True:
         # Accept incoming connections and spawn a new thread to handle each one
         client_socket, client_address = server_socket.accept()
-        thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        thread = threading.Thread(
+            target=handle_client, args=(
+                client_socket, client_address))
         thread.start()
+
 
 # Start the netcat listener in a separate thread
 threading.Thread(target=start_netcat_listener, args=(PORT_NUMBER,)).start()
 
 # This route serves the index.html template
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 # This SocketIO event handler runs when a client connects to the server
+
+
 @socketio.on("connect")
 def handle_connect():
     # Send a "Connected" message to the connected client using SocketIO
     emit("message", "Connected")
+
 
 # Start the Flask app with SocketIO support
 if __name__ == "__main__":
