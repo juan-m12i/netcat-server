a48766dc5e57f7467da8f205f1dfa8792343d9ca
Juan <juanmontalvobressi@hotmail.com>
2023-04-14 15:39:38 -0300

    Solving small issue and making it loook better

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,25 @@
-const socket = io();
-const terminal = document.getElementById('terminal');
+document.addEventListener("DOMContentLoaded", function () {
+  const socket = io.connect(location.protocol + "//" + document.domain + ":" + location.port);
+  const terminalOutput = document.getElementById("terminal-output");
+  const terminalInput = document.getElementById("terminal-input");
 
-socket.on('data', (data) => {
-  terminal.textContent += data.message + '
-';
-  terminal.scrollTop = terminal.scrollHeight;
+  socket.on("connect", function () {
+    socket.emit("user_connected");
+  });
+
+  socket.on("message", function (msg) {
+    terminalOutput.innerHTML += msg + "\n";
+    terminalOutput.scrollTop = terminalOutput.scrollHeight;
+  });
+
+  terminalInput.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) { // Enter key
+      event.preventDefault();
+      const inputValue = event.target.value.trim();
+      if (inputValue) {
+        socket.emit("filter_data", inputValue);
+      }
+      event.target.value = "";
+    }
+  });
 });
-
