a6c127d67c3c1a52795a3053d521f136d71b3a93
juan-m12i <juanmontalvobressi@hotmail.com>
2023-04-18 12:23:49 -0300

    dealing with rf

--- build_and_run_docker.sh
+++ build_and_run_docker.sh
@@ -1,1 +1,12 @@
-CONTAINER_NAME="cont-netcat-web"IMAGE_NAME="img-netcat-web"docker build -t $IMAGE_NAME .# Remove the container if it already existsif docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then  docker stop $CONTAINER_NAME  docker rm $CONTAINER_NAMEfidocker run -d --name $CONTAINER_NAME -p 5010:5010 -p 12345:12345 $IMAGE_NAME
+CONTAINER_NAME="cont-netcat-web"
+IMAGE_NAME="img-netcat-web"
+
+docker build -t $IMAGE_NAME .
+
+# Remove the container if it already exists
+if docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
+  docker stop $CONTAINER_NAME
+  docker rm $CONTAINER_NAME
+fi
+
+docker run -d --name $CONTAINER_NAME -p 5010:5010 -p 12345:12345 $IMAGE_NAME
